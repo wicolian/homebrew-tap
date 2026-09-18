3e499e772bced73282739822fdf9a279cf8ee351
wicolian <136704438+wicolian@users.noreply.github.com>
2026-09-18 01:13:59 +0000

    boxdeck 0.3.0

--- Casks/boxdeck-bar.rb
+++ Casks/boxdeck-bar.rb
@@ -1,11 +1,16 @@
 cask "boxdeck-bar" do
-  version "0.2.0"
-  sha256 "2415fd83638adabcfbb05eed856041a28f0153cdc0ed82baf1796a72e0bc76f0"
+  version "0.3.0"
+  sha256 "c11a2547e82b0826d1a50e17a4e56eab026a96afe7e754fb18eb4f65e0dc423a"
 
   url "https://github.com/wicolian/boxdeck/releases/download/v#{version}/boxdeck-bar_darwin_universal.zip"
   name "boxdeck-bar"
   desc "Cross-platform menu bar client for boxdeck"
   homepage "https://github.com/wicolian/boxdeck"
+
+  livecheck do
+    url "https://github.com/wicolian/boxdeck"
+    strategy :github_latest
+  end
 
   app "boxdeck-bar.app"
 end
--- Formula/boxdeck.rb
+++ Formula/boxdeck.rb
@@ -1,30 +1,35 @@
 class Boxdeck < Formula
   desc "Live console for a remote dev box"
   homepage "https://github.com/wicolian/boxdeck"
-  version "0.2.0"
+  version "0.3.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/wicolian/boxdeck/releases/download/v#{version}/boxdeck_darwin_arm64"
-      sha256 "5633d8674d7ee7e163cd96692d786b878a0be9411f8b8abbc829437faeca43ee"
+      sha256 "ab002d6250f555f5d68b8513369e374d8389b1a33f5ccdd500e52a316923f340"
     else
       url "https://github.com/wicolian/boxdeck/releases/download/v#{version}/boxdeck_darwin_amd64"
-      sha256 "bb56a761910807498d7ec9d18fd6fdda65f70efaf29bca2d3d887e070bb99d2f"
+      sha256 "45718bd3646b41b71e55860abf3cdf350e862c30e05a0e56a8355cb82a8cf246"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/wicolian/boxdeck/releases/download/v#{version}/boxdeck_linux_arm64"
-      sha256 "d8cbde4acb554791080e072bd77d8f4c5b3adb895fcae11076fb6e05ccfd5187"
+      sha256 "c49fbb61c5bd312c92d0242a6a01eaa1f9363efc6ba6ca73014fbdd1154cebdd"
     else
       url "https://github.com/wicolian/boxdeck/releases/download/v#{version}/boxdeck_linux_amd64"
-      sha256 "84f3e3989854e8dc967bc969ad2a74f9555fcb7ee391a2d492eae396ffdad12f"
+      sha256 "eda4abb09a9cbc01fe1a924f3e00ecf204322d766ab9f5c3e0bd14a38e08b38a"
     end
   end
 
   def install
     bin.install Dir["boxdeck*"][0] => "boxdeck"
+  end
+
+  livecheck do
+    url "https://github.com/wicolian/boxdeck"
+    strategy :github_latest
   end
 
   service do
